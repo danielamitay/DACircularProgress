1e1974fc790bb27efbbfc286287908891d1e23f3
Josh Sklar <jsklar@detroitlabs.com>
2014-04-08 00:04:21 -0400

    add DALabeledCircularProgressView

---
+++ DACircularProgress/DALabeledCircularProgressView.h
@@ -0,0 +1,13 @@
+//
+//  DALabeledCircularProgressView.h
+//  DACircularProgressExample
+//
+//  Created by Josh Sklar on 4/8/14.
+//  Copyright (c) 2014 Shout Messenger. All rights reserved.
+//
+
+#import "DACircularProgressView.h"
+
+@interface DALabeledCircularProgressView : DACircularProgressView
+
+@end
---
+++ DACircularProgress/DALabeledCircularProgressView.m
@@ -0,0 +1,31 @@
+//
+//  DALabeledCircularProgressView.m
+//  DACircularProgressExample
+//
+//  Created by Josh Sklar on 4/8/14.
+//  Copyright (c) 2014 Shout Messenger. All rights reserved.
+//
+
+#import "DALabeledCircularProgressView.h"
+
+@implementation DALabeledCircularProgressView
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
